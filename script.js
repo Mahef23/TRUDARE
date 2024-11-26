b002025bba8bb46f0d30aa9300dfe2322155f5f3
Mahef23 <monkeygidr0@gmail.com>
2024-11-26 17:15:57 +0530

    1126

--- script.js
+++ script.js
@@ -1,23 +1,55 @@
 joueur=document.querySelector('.Joueur')
-let nomDeJouer
-document.querySelector('.lalao').addEventListener('click',function(e){
+let play=document.querySelector('#play')
+function jeu(){
+play.addEventListener('click',function(e){
     e.preventDefault();
-    
-    let jeu=['Paul','Armane','Beva']
-    for (let i = 0; i<jeu.length ; i++){
-        let isanajouer= document.createElement('button');
-        isanajouer.textContent=jeu[i];
-        isanajouer.id=jeu[i];
-    joueur.appendChild(isanajouer);
-    }
-    boutton=document.querySelector('button')
-    boutton.addEventListener('click',function(o){
-        o.preventDefault()
-         nomDeJouer = boutton.target.id
-         document.write(nomDeJouer)
-        /* let texte= document.createElement('p')
-         texte.textContent=nomDeJouer
-         joueur.appendChild(texte)*/
+    let nomDeJouer=document.createElement('p')
+    nomDeJouer.textContent= "Nom du Joueur"
+    play.disabled=true;
+
+    let trudare=document.createElement('button')
+    trudare.textContent="Truth or Dare"
+    trudare.id="boutonTD"
+    joueur.appendChild(nomDeJouer);
+    joueur.appendChild(trudare)
+    trudare.classList.add('truedare')
+    let act_image=document.createElement('img')
+        act_image.src='/down-modified.png'
+        act_image.style.height='50px'
+        trudare.appendChild(act_image)
+
+    trudare.addEventListener('click',function(e){
+        e.preventDefault()
+        let div_act=document.createElement('div')
+        div_act.class='division'
+
+        let actionverite=document.createElement('p')
+        actionverite.textContent='ACTION OU VERITE'
+        actionverite.id='textes'
+        joueur.appendChild(actionverite)
+        trudare.disabled=true
+
+        
+        /*div_act.appendChild(actionverite)
+        div_act.appendChild(act_image)
+        
+        joueur.appendChild(div_act)*/
+
+        let rejoue=document.createElement('button')
+        rejoue.textContent='CONTINUE'
+        rejoue.classList.add('rejoue')
+        joueur.appendChild(rejoue)
+       let act_image2=document.createElement('img')
+        act_image2.src='/left-modified.png'
+        act_image2.style.height='50px'
+        rejoue.appendChild(act_image2)
+        rejoue.addEventListener('click',function(e){
+            e.preventDefault()
+            joueur.innerHTML=''
+            play.disabled= false
+            trudare.disabled=false
+        })
     })
 })
-
+}
+jeu()
