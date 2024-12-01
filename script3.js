4a3db8b9b1741b64596b048350de65749f25bb93
Mahef23 <monkeygidr0@gmail.com>
2024-12-01 13:01:53 +0530

    1201

--- script3.js
+++ script3.js
@@ -1,18 +1,27 @@
 joueur=document.querySelector('.Joueur')
 let play=document.querySelector('#play')
-let i=0
+
 function jeu(){
 play.addEventListener('click',function(e){
     e.preventDefault();
-    let nom=['Paul','Armane','Beva','Patrick','Sarobidy','Tsiky']
-    let olona=Math.floor(Math.random()*6)
-    let anarana=nom[olona]
-    let tableau=[]
+    let tableau =[]
+    let nom=['Paul','Armane','Beva','Patrick','Sarobidy','Tsiky','Dayan','Nathan']
+    function choixnum(){
+    return Math.floor(Math.random()*nom.length);
+}
+    for (let i=0 ; i<nom.length ; i++){
+        let num
+        do{
+            num=choixnum();
+        }while(tableau.includes(num))
+            tableau.push(num)
+    }
+    let anarana=nom[choixnum()]
+   
 
     let nomDeJouer=document.createElement('p')
     nomDeJouer.textContent= anarana
     play.disabled=true;
-    console.log(tableau[i])
     let trudare=document.createElement('button')
     trudare.textContent="Truth or Dare"
     trudare.id="boutonTD"
@@ -28,17 +37,30 @@ play.addEventListener('click',function(e){
         e.preventDefault()
         let div_act=document.createElement('div')
         div_act.class='division'
-        let act_ver=["Frappe une personne de meme sexe que toi ici",
+        let choi_act_ver= (Math.floor(Math.random()*1000))
+        let dif_ver=["Dis nous un moment tellement genant que tu as vecu",
+            "Avoue si tu es en colère avec une personne ici",
+            "Y a-t-il une personne de different sexe que toi que tu préfères",
+            "Raconte nous ta première fois",
+            "Voudrais-tu que cette personne(Pointer une personne) sorte avec toi"
+        ]
+        let dif_act=["Frappe une personne de meme sexe que toi ici",
             "Rentre chez toi et appel nous en video de ta chambre avec une preuve",
             "Prend ton telephone et dis bonjour à une personne que tu n'as plus parler depuis longtemps",
             "Va au Gate et prend un selfie puis tu reviens",
             "Va dire bonjour à une personne dans ce parc puis reviens sans rien dire de plus"
         ]
-    
-        let act_ver_random= Math.floor(Math.random()*5)
         let actionverite=document.createElement('p')
-        actionverite.textContent= act_ver[act_ver_random]
-        actionverite.id='textes'
+        let act_ver_random=Math.floor(Math.random()*4)
+        if (choi_act_ver%2==0){
+            actionverite.innerHTML="Vérité <br> "+dif_ver[act_ver_random]
+            actionverite.id='textes'    
+        }
+        else{
+            actionverite.innerHTML="Action <br>"+dif_act[act_ver_random]
+            actionverite.id='textes'    
+        }
+        
         joueur.appendChild(actionverite)
         trudare.disabled=true
 
@@ -64,6 +86,5 @@ play.addEventListener('click',function(e){
         })
     })
 })
-i++
 }
 jeu()
